6c27bd1cc41b0d6e571e862a08a3f0fe49c3db04
gustavo <gustavo.simoes@hotmail.com.br>
2019-02-02 12:59:55 -0200

    Correção do nome da entidade, pois 'order' é palavra reservada do banco de dados

--- src/main/java/br/com/gustavosimoes/magazine/model/Order.java
+++ src/main/java/br/com/gustavosimoes/magazine/model/Order.java
@@ -5,9 +5,11 @@ import lombok.Data;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Data
 @Entity
+@Table(name = "orders")
 public class Order {
 
     @Id
